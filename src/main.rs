ce4bafce30ccace6e2a1b6f5cd178015a6a89eea
Caspar Krieger <git@asparck.com>
2021-09-19 14:32:40 +0800

    Color sphere according to surface normal

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,28 @@ use ray::Ray;
 use rgb::RGB8;
 use vec3::{lerp, Color, Point3, Vec3};
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let circle_center = Point3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(circle_center, 0.5, r);
+    if t > 0.0 {
+        let n = r.at(t) - circle_center;
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
     }
+
     let unit_direction = r.direction().to_unit();
     let t = 0.5 * (unit_direction.y + 1.0);
     let ground: Color = Color::new(1.0, 1.0, 1.0);
